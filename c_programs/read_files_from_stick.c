3bae7ece93bb32a017d012fe86a69f0343974db8
Hearues Zueke <hziko314@gmail.com>
2017-04-26 15:37:55 +0200

    add writeat function

--- c_programs/read_files_from_stick.c
+++ c_programs/read_files_from_stick.c
@@ -1,6 +1,7 @@
 #include "read_files_from_stick.h"
 
 char current_path[1024];
+char file_path[1024];
 
 int read_input(char* input, char** commands, int* idx) {
     memset(input, 0, INPUT_LENGTH);
@@ -36,6 +37,16 @@ int read_input(char* input, char** commands, int* idx) {
     return 0;
 }
 
+void print_error_message(int error_ret) {
+    switch (error_ret) {
+        case 1:
+            printf(AC_RED_BOLD "Error:" AC_RESET " max char length: %d\n", INPUT_LENGTH);
+        case 2:
+            printf(AC_RED_BOLD "Error:" AC_RESET " not more than %d commands!\n", COMMANDS);
+    }
+
+}
+
 void get_normal_input(int idx, char* input, char* input_copy, char** commands) {
     char* c = commands[idx-1];
     while (*c != 0) { c++; }
@@ -48,6 +59,32 @@ void get_normal_input(int idx, char* input, char* input_copy, char** commands) {
             *c1 = ' ';
         }
     } while (c1 != input_copy);
+}
+
+CommandNumber get_command_number(char* command_0) {
+    if (!strcmp(command_0, "exit") || !strcmp(command_0, "e")) {
+        return EXIT;
+    } else if (!strcmp(command_0, "newprogram") || !strcmp(command_0, "n")) {
+        return NEWPROGRAM;
+    } else if (!strcmp(command_0, "defines")) {
+        return DEFINES;
+    } else if (!strcmp(command_0, "setfile")) {
+        return SETFILE;
+    } else if (!strcmp(command_0, "driver")) {
+        return DRIVER;
+    } else if (!strcmp(command_0, "getsector") || !strcmp(command_0, "gsec")) {
+        return GETSECTOR;
+    } else if (!strcmp(command_0, "writeat") || !strcmp(command_0, "w")) {
+        return WRITEAT;
+    } else if (!strcmp(command_0, "printargs")) {
+        return PRINTARGS;
+    } else if (!strcmp(command_0, "cd")) {
+        return CD;
+    } else if (!strcmp(command_0, "help") || !strcmp(command_0, "h")) {
+        return HELP;
+    }
+
+    return BASH;
 }
 
 int is_number_decimal(char* c) {
@@ -74,6 +111,142 @@ int is_number_hex(char* c) {
     return 1;
 }
 
+void command_newprogram() {
+    system("make");
+    char program_name[] = "./read_files_from_stick";
+    execl(program_name, program_name, NULL);
+}
+
+void command_defines() {
+    printf("INPUT_LENGTH: %d\n", INPUT_LENGTH);
+    printf("COMMANDS    : %d\n", COMMANDS);
+}
+
+void command_setfiles(char** commands, int idx) {
+    if (idx < 2) {
+        printf(AC_RED_BOLD "Error:" AC_RESET " need 1 argument: " AC_GREEN "file_path" AC_RESET "\n");
+    }
+    sprintf(file_path, "%s", commands[1]);
+}
+
+void command_driver(char** commands, int idx) {
+    if (idx < 2) {
+        printf(AC_RED_BOLD "Error:" AC_RESET " need 1 argument: " AC_GREEN "driver_name" AC_RESET "\n");
+    }
+    sprintf(file_path, "/dev/%s", commands[1]);
+}
+
+void command_getsector(char** commands, int idx) {
+    int lba = 0;
+    if (is_number_decimal(commands[1])) {
+        lba = strtol(commands[1], NULL, 10);
+    } else if (is_number_hex(commands[1])) {
+        lba = strtol(commands[1], NULL, 16);
+    } else {
+        printf(AC_RED_BOLD "Error:" AC_RESET " 1st argument is not a decimal or hex number!\n");  
+    }
+
+    FILE* f_drive = fopen(file_path, "rb");
+    if (f_drive == NULL) {
+        printf(AC_RED_BOLD "Error:" AC_RESET " file " AC_GREEN "%s" AC_RESET " does not exist\n", file_path);
+        return;
+    }
+    uint8_t block[SECTOR_SIZE];
+    fseek(f_drive, SECTOR_SIZE*lba, SEEK_SET);
+    fread((void*)block, 1, SECTOR_SIZE, f_drive);
+    fclose(f_drive);
+    printSector(block, SECTOR_SIZE, 16, lba);
+}
+
+void command_writeat(char** commands, int idx) {
+    if (idx < 3) {
+        printf(AC_RED_BOLD "Error:" AC_RESET " 2 arguments needed! (first: pos, second: byte value)\n"); 
+        return; 
+    }
+
+    int pos = 0;
+    uint8_t byte = 0;
+
+    if (is_number_decimal(commands[1])) {
+        pos = strtol(commands[1], NULL, 10);
+    } else if (is_number_hex(commands[1])) {
+        pos = strtol(commands[1], NULL, 16);
+    } else {
+        printf(AC_RED_BOLD "Error:" AC_RESET " first argument is not a decimal number!\n");  
+        return;
+    }
+
+    if (is_number_decimal(commands[2])) {
+        byte = strtol(commands[2], NULL, 10) % 0x100;
+    } else if (is_number_hex(commands[1])) {
+        byte = strtol(commands[2], NULL, 16) % 0x100;
+    } else {
+        printf(AC_RED_BOLD "Error:" AC_RESET " second argument is not a decimal number!\n");
+        return;
+    }
+    FILE* f_drive = fopen(file_path, "wb");
+    fseek(f_drive, pos, SEEK_SET);
+    fwrite((void*)&byte, 1, 1, f_drive);
+    fclose(f_drive);
+    printf("Successfully written at pos " AC_GREEN "0x%X" AC_RESET " byte " AC_GREEN "0x%X" AC_RESET " in file " AC_GREEN "%s" AC_RESET "\n", pos, byte, file_path);
+}
+
+void command_printargs(int argc, char** argv) {
+    printf("argc: %d\n", argc);
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+}
+
+void command_cd(char** commands, int idx) {
+    if (idx < 2) {
+        printf(AC_RED_BOLD "Error:" AC_RESET " command " AC_GREEN "cd" AC_RESET " takes one argument\n");
+    }
+    if (!strcmp(commands[1], ".")) {
+        return;
+    } else if (!strcmp(commands[1], "..")) {
+        char* c = current_path;
+        while (*c != 0) { c++; }
+
+        while (*c != '/') { *c = 0; c--; }
+        if (c != current_path) {
+            *c = 0;
+        }
+        chdir(current_path);
+    } else {
+        char* c = current_path;
+        while (*c != 0) { c++; }
+        char* save_pos = c;
+        if (c-1 != current_path) {
+            *c = '/';
+            c++;
+        }
+        sprintf(c, "%s", commands[1]);
+        if (chdir(current_path)) {
+            printf(AC_RED_BOLD "Error:" AC_RESET " folder %s does not exist\n", commands[1]);
+            while (*c != 0) { c++; }
+            while (c != save_pos) { *c = 0; c--; } *c = 0;
+        }
+    }
+}
+
+void command_print() {
+    printf(AC_MAGENTA "commands:\n" AC_RESET);
+    printf(AC_GREEN "defines           " AC_RESET "Print all given defines\n");
+    printf(AC_GREEN "printargs         " AC_RESET "Print the given argc and argv parameters\n");
+    printf(AC_GREEN "setfile           " AC_RESET "Set the path to the file (absolute)\n");
+    printf(AC_GREEN "driver            " AC_RESET "Set the path to the file (" AC_CYAN "/dev/" AC_RESET " as preffix)\n");
+    printf(AC_GREEN "getsector, gsec   " AC_RESET "Prints the sector block with the given " AC_CYAN "index" AC_RESET " ("AC_YELLOW"decimal"AC_RESET" or "AC_YELLOW"hex"AC_RESET" value)\n");
+    printf(AC_GREEN "cd                " AC_RESET "Change directory, possible inputs: "
+        AC_CYAN "." AC_RESET ", "
+        AC_CYAN ".." AC_RESET ", "
+        AC_CYAN "<one_folder>" AC_RESET " (this is not bash command!)\n");
+    printf(AC_GREEN "exit"AC_RESET", "AC_GREEN"e"AC_RESET"           Terminate the program\n");
+    printf(AC_GREEN "other             " AC_RESET "And other normal commmands, what your " AC_YELLOW "bash" AC_RESET " can do ;-)\n");
+    printf(AC_GREEN "help" AC_RESET ", " AC_GREEN "h" AC_RESET "           " AC_RESET "Print this help\n");
+    
+}
+
 /*
 example for string concatination:
     char input[128];
@@ -87,7 +260,7 @@ example for string concatination:
 int main(int argc, char* const argv[]) {
     system("reset");
     getcwd(current_path, sizeof(current_path));
-    char file_path[1024] = {0}; // = "/dev/sdb"; // the usb stick
+    memset(file_path, 0, 1024); // e.g.
 
     char input[INPUT_LENGTH];
     char input_copy[INPUT_LENGTH];
@@ -98,127 +271,54 @@ int main(int argc, char* const argv[]) {
         int idx = 0;
         int error_ret = read_input(input, commands, &idx);
 
-        switch (error_ret) {
-            case 1:
-                printf(AC_RED_BOLD "Error:" AC_RESET " max char length: %d\n", INPUT_LENGTH);
-                continue;
-            case 2:
-                printf(AC_RED_BOLD "Error:" AC_RESET " not more than %d commands!\n", COMMANDS);
-                continue;
+        if (error_ret) {
+            print_error_message(error_ret);
+            continue;
         }
 
         get_normal_input(idx, input, input_copy, commands);
 
-        // TODO: need to move this to an other function!
-        if (!strcmp(commands[0], "exit") ||
-            !strcmp(commands[0], "e")) {
-            printf("Exiting program...\n");
+        CommandNumber cn = get_command_number(commands[0]);
+        int is_exiting_program = 0;
+        switch (cn) {
+            case EXIT:
+                is_exiting_program = 1;
+                break;
+            case NEWPROGRAM:
+                command_newprogram();
+                break;
+            case DEFINES:
+                command_defines();
+                break;
+            case SETFILE:
+                command_setfiles(commands, idx);
+                break;
+            case DRIVER:
+                command_driver(commands, idx);
+                break;
+            case GETSECTOR:
+                command_getsector(commands, idx);
+                break;
+            case WRITEAT:
+                command_writeat(commands, idx);
+                break;
+            case PRINTARGS:
+                command_printargs(argc, (void*)argv);
+                break;
+            case CD:
+                command_cd(commands, idx);
+                break;
+            case HELP:
+                command_print();
+                break;
+            default: // BASH
+                system(input_copy);
+        }
+
+        if (is_exiting_program) {
             break;
-        } else if (!strcmp(commands[0], "n")) {
-            system("make");
-            char program_name[] = "./read_files_from_stick";
-            execl(program_name, program_name, NULL);
         }
-        else if (!strcmp(commands[0], "defines")) {
-            printf("INPUT_LENGTH: %d\n", INPUT_LENGTH);
-            printf("COMMANDS    : %d\n", COMMANDS);
-        }
-        else if (!strcmp(commands[0], "setfile")) {
-            if (idx < 2) {
-                printf(AC_RED_BOLD "Error:" AC_RESET " need 1 argument: " AC_GREEN "file_path" AC_RESET "\n");
-                continue;
-            }
-            sprintf(file_path, commands[1]);
-        }
-        else if (!strcmp(commands[0], "driver")) {
-            if (idx < 2) {
-                printf(AC_RED_BOLD "Error:" AC_RESET " need 1 argument: " AC_GREEN "driver_name" AC_RESET "\n");
-                continue;
-            }
-            sprintf(file_path, "/dev/%s", commands[1]);
-        }
-        else if (!strcmp(commands[0], "getsector") ||
-                 !strcmp(commands[0], "gsec")) {
-            int lba = 0;
-            if (idx >= 2) {
-                if (is_number_decimal(commands[1])) {
-                    lba = strtol(commands[1], NULL, 10);
-                } else if (is_number_hex(commands[1])) {
-                    lba = strtol(commands[1], NULL, 16);
-                } else {
-                    printf(AC_RED_BOLD "Error:" AC_RESET " second argument is not a decimal number!\n");
-                    continue;   
-                }
-            }
-            FILE* f_drive = fopen(file_path, "rb");
-            if (f_drive == NULL) {
-                printf(AC_RED_BOLD "Error:" AC_RESET " file " AC_GREEN "%s" AC_RESET " does not exist\n", file_path);
-                continue;
-            }
-            uint8_t block[SECTOR_SIZE];
-            fseek(f_drive, SECTOR_SIZE*lba, SEEK_SET);
-            fread((void*)block, 1, SECTOR_SIZE, f_drive);
-            fclose(f_drive);
-            printSector(block, SECTOR_SIZE, 16, lba);
-        }
-        else if (!strcmp(commands[0], "printargs")) {
-            printf("argc: %d\n", argc);
-            for (int i = 0; i < argc; i++) {
-                printf("argv[%d] = %s\n", i, argv[i]);
-            }
-        }
-        else if (!strcmp(commands[0], "cd")) {
-            if (idx < 2) {
-                printf(AC_RED_BOLD "Error:" AC_RESET " command " AC_GREEN "cd" AC_RESET " takes one argument\n");
-                continue;
-            }
-            if (!strcmp(commands[1], ".")) {
-                continue;
-            } else if (!strcmp(commands[1], "..")) {
-                char* c = current_path;
-                while (*c != 0) { c++; }
 
-                while (*c != '/') { *c = 0; c--; }
-                if (c != current_path) {
-                    *c = 0;
-                }
-                chdir(current_path);
-
-                continue;
-            } else {
-                char* c = current_path;
-                while (*c != 0) { c++; }
-                char* save_pos = c;
-                if (c-1 != current_path) {
-                    *c = '/';
-                    c++;
-                }
-                sprintf(c, "%s", commands[1]);
-                if (chdir(current_path)) {
-                    printf(AC_RED_BOLD "Error:" AC_RESET " folder %s does not exist\n", commands[1]);
-                    while (*c != 0) { c++; }
-                    while (c != save_pos) { *c = 0; c--; } *c = 0;
-                }
-                continue;
-            }
-        } else if (!strcmp(commands[0], "help") ||
-                   !strcmp(commands[0], "h")) {
-            printf(AC_MAGENTA "commands:\n" AC_RESET);
-            printf(AC_GREEN "defines           " AC_RESET "Print all given defines\n");
-            printf(AC_GREEN "printargs         " AC_RESET "Print the given argc and argv parameters\n");
-            printf(AC_GREEN "setfile           " AC_RESET "Set the path to the file (absolute)\n");
-            printf(AC_GREEN "driver            " AC_RESET "Set the path to the file (" AC_CYAN "/dev/" AC_RESET " as preffix)\n");
-            printf(AC_GREEN "getsector, gsec   " AC_RESET "Prints the sector block with the given " AC_CYAN "index" AC_RESET " ("AC_YELLOW"decimal"AC_RESET" or "AC_YELLOW"hex"AC_RESET" value)\n");
-            printf(AC_GREEN "cd                " AC_RESET "Change directory, possible inputs: "
-                AC_CYAN "." AC_RESET ", "
-                AC_CYAN ".." AC_RESET ", "
-                AC_CYAN "<one_folder>" AC_RESET " (this is not bash command!)\n");
-            printf(AC_GREEN "exit"AC_RESET", "AC_GREEN"e"AC_RESET"           Terminate the program\n");
-            printf(AC_GREEN "other             " AC_RESET "And other normal commmands, what the your " AC_YELLOW "bash" AC_RESET " can do ;-)\n");
-            printf(AC_GREEN "help" AC_RESET ", " AC_GREEN "h" AC_RESET "           " AC_RESET "Print this help\n");
-        } else {
-            system(input_copy);
-        }
     }
 
     return 0;
--- c_programs/read_files_from_stick.h
+++ c_programs/read_files_from_stick.h
@@ -5,9 +5,40 @@
 
 #include "file_system_utils.h"
 
+typedef enum {
+    EXIT,
+    NEWPROGRAM,
+    DEFINES,
+    SETFILE,
+    DRIVER,
+    GETSECTOR,
+    WRITEAT,
+    PRINTARGS,
+    CD,
+    HELP,
+    BASH
+} CommandNumber;
+
 int read_input(char* input, char** commands, int* idx);
 void get_normal_input(int idx, char* input, char* input_copy, char** commands);
 int is_number_decimal(char* c);
 int is_number_hex(char* c);
+
+int read_input(char* input, char** commands, int* idx);
+void print_error_message(int error_ret);
+void get_normal_input(int idx, char* input, char* input_copy, char** commands);
+
+CommandNumber get_command_number(char* command_0);
+int is_number_decimal(char* c);
+int is_number_hex(char* c);
+
+void command_newprogram();
+void command_defines();
+void command_setfiles(char** commands, int idx);
+void command_driver(char** commands, int idx);
+void command_getsector(char** commands, int idx);
+void command_printargs(int argc, char** argv);
+void command_cd(char** commands, int idx);
+void command_print();
 
 #endif /* READ_FILES_FROM_STICK */
